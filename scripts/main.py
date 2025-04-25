fe145445e4e8b0f91e45614e427e200726bf1ac0
Ayush-Sadekar <ayushs@vt.edu>
2025-04-25 19:07:57 -0400

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -8,3 +8,13 @@ import chromadb
 from sentence_transformers import SentenceTransformer
 
 from scraper import scrape_vt_dining_locations, write_dining_file, get_hours
+
+dining_halls = scrape_vt_dining_locations("https://foodpro.students.vt.edu/menus/")
+dir_path = "/Users/ayush/Desktop/BeautSoupCrawlerDining/scripts"
+
+file_paths = []
+
+for hall in dining_halls:
+    file_paths.append(write_dining_file(hall, dir_path))
+
+
