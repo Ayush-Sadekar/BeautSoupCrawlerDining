b0d2333e046b3b339d33a2a9196f7effdbe9f2f6
Ayush-Sadekar <ayushs@vt.edu>
2025-05-16 18:58:19 -0700

    some more changes

--- scripts/new_main.py
+++ scripts/new_main.py
@@ -56,7 +56,6 @@ else:
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
     return render_template('index.html')
 
@@ -72,7 +71,7 @@ def query():
 
 if __name__ == '__main__':
 
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5050))
 
     app.run(host='0.0.0.0', port=port, debug=False)
 
